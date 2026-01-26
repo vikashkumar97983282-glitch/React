ed737f0fc953a104cf9404245f5b81fe25c900ad
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-01-26 23:10:19 +0530

    this is component folder.

---
+++ projects/src/component/container.jsx
@@ -0,0 +1,17 @@
+import "./container.css"
+
+import React from "react";
+
+function First(){
+    return (
+        <div className="cont">
+        <img src="https://images.unsplash.com/photo-1747409020057-23556b36b38a?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        <h1>Vikash Kumar</h1>
+        <h2>B Tech (computer engineer)</h2>
+        <p>I am interested in AI/ML</p>
+        <button>view profile</button>
+        </div>
+    )
+}
+
+export default First;
---
+++ projects/src/component/navbar.jsx
@@ -0,0 +1,16 @@
+import "./navbar.css";
+
+
+
+
+function Navbar(){
+    return (
+        <>
+        <div className='nav'>
+            <h1>NAVBAR</h1>
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
