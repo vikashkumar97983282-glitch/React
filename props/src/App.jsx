bd47fc58b87094c8e5b17b6d42f0404459f7b3be
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-01-29 01:01:24 +0530

    change some properties in this file.

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -13,7 +13,7 @@ const App = ()=> {
       dept: "B Tech (computer engineer)"
     },
     {
-      user:"Rahul Kumar",
+      user:"Rahul ",
       skill: "Frontend Developers",
       img : "https://plus.unsplash.com/premium_photo-1675793715030-0584c8ec4a13?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       dept: "B Tech (computer engineer)"
