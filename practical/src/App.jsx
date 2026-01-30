6ad851d6638dc12902f0b10a3cbda83204dc531c
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-01-31 01:09:20 +0530

    this is hooks (useState) repos.

---
+++ practical/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import HooksExample from './hooks/hooks'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HooksExample/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ practical/src/hooks/hooks.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+
+function HooksExample(){
+
+    const [first, setFirst] = useState("");
+    const [last, setLast] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [data,setData] = useState([])
+   
+
+    const addcontact= (e)=>{
+        e.preventDefault();
+        const copyData=[...data];
+        copyData.push({first,last,mobile})
+        
+        setData(copyData)
+
+        console.log(copyData)
+
+    }
+
+    const deletecont=(idx)=>{
+        const copyData =[...data]
+
+        console.log(copyData[idx])
+
+        copyData.splice(idx,1)
+
+        setData(copyData)
+
+    }
+
+
+
+
+
+
+    return (
+        <div>
+            <form onClick={(e)=>{e.preventDefault()}}>
+                <input type="text" placeholder="first name" value={first} onChange={(e)=>setFirst(e.target.value)}/>
+                <br/>
+                <input type="text" placeholder="last name" value={last} onChange={(e)=>setLast(e.target.value)}/>
+                <br/>
+                <input type="moblie number" placeholder="Mobile number" value={mobile} onChange={(e)=>setMobile(e.target.value)}/>
+                <br/>
+                <button onClick={addcontact} >add</button>
+                <ul>
+                    {data.map(function(ele,idx){
+                        return <div key={idx}>
+                            <li>{ele.first}</li>
+                            <button onClick={(e)=>{e.preventDefault()}}>show</button>
+                            <button onClick={()=>{
+                                deletecont(idx)
+                            }}>delete</button>
+                        </div>
+                    })}
+                </ul>
+                
+            </form>
+        </div>
+    )
+}
+
+export default HooksExample;
