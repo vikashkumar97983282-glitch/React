4b256493edecf38160d7eb6ee6ee4fc11bdcd186
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-01-31 02:42:37 +0530

    complete the login page .

--- practical/src/App.jsx
+++ practical/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import HooksExample from './hooks/hooks'
 
 
--- project/src/Login/login.jsx
+++ project/src/Login/login.jsx
@@ -1,27 +1,50 @@
 import React from "react";
 import './login.css'
+import { useState } from "react";
 
 function Login(){
 
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [message,setMessage] = useState("")
+
+    const handlelogin = (e)=>{
+
+        e.preventDefault()
+
+        if(username === "abc@gmail.com" && password === "1234"){
+            setMessage("login sucessfully !");
+        } else {
+            setMessage("login failed !");
+        }
+
+        setUsername("")
+        setPassword("")
+    
+    }
+
     return (
-        <div className="login">
-            <div className="content">
-                {/* <h1>CUSTOMER </h1> */}
-                <div className="logo">
-                    <img src="https://www.citypng.com/public/uploads/preview/hd-man-user-illustration-icon-transparent-png-701751694974843ybexneueic.png" alt="" />
+        <div className="container">
+            <form onSubmit={handlelogin}>
+                <div className="content">
+                    {/* <h1>CUSTOMER </h1> */}
+                    <div className="logo">
+                        <img src="https://www.citypng.com/public/uploads/preview/hd-man-user-illustration-icon-transparent-png-701751694974843ybexneueic.png" alt="" />
+                    </div>
+                    <br/>
+                    <input className="data" value={username} type="email" id="email" name="email" placeholder="Enter Email" required onChange={(e)=>setUsername(e.target.value)}/>
+                    <br/>
+                    <input className="data" value={password} type="password" id="password" name="password" placeholder="Enter Password" required onChange={(e)=>setPassword(e.target.value)}/>
+                    <br/>
+                    <input className="box" id="check" type="checkbox"/>
+                    <label htmlFor="check">Remember me</label>
+                    <a href="#">Forget password</a>
+                    <br/>
+                    <br/>
+                    <button type="submit">Login</button>
+                    <p>{message}</p>
                 </div>
-                <br/>
-                <input className="data" type="email" id="email" name="email" placeholder="Enter Email"/>
-                <br/>
-                <input className="data" type="password" id="password" name="password" placeholder="Enter Password" required/>
-                <br/>
-                <input className="box" id="check" type="checkbox" placeholder="Remember me"/>
-                <label for="check">Remember me</label>
-                <a href="">Forget password</a>
-                <br/>
-                <br/>
-                <button >Login</button>
-            </div>
+            </form>
         </div>
     )
 }
