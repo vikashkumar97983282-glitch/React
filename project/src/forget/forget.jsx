bb41dcdafcf2c5fd995fc56322d545ef5dd9be66
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-02-02 16:12:14 +0530

    add functionality in forgot password

--- project/src/forget/forget.jsx
+++ project/src/forget/forget.jsx
@@ -5,25 +5,37 @@ function Forget(){
 
     const [newpass,setNewpass] = useState("");
     const [confirm,setConfirm] = useState("");
+    const [mes,setMes] = useState("")
 
     const clk = (e)=>{
         e.preventDefault()
-        setNewpass(newpass)
-        setConfirm(confirm)
+
+        if (newpass !== "" && newpass === confirm){
+            setNewpass("")
+            setConfirm("")
+            setMes("password update sucessfully")
+        }
+        else {
+            setConfirm("")
+            setMes("confirm password incorrect")
+        }
+
+        
     }
 
     return(
         <div className="forg">
-            <form onClick={clk}>
+            <form >
                 <div className="forcont">
                     <div className="image">
                         <img src="https://cdn-icons-png.flaticon.com/512/6195/6195699.png" alt="" />
                     </div>
-                    <input type="password" placeholder="new password" required onChange={(e)=>setNewpass(e.target.value)}/>
+                    <input type="password" value={newpass} placeholder="new password" required onChange={(e)=>setNewpass(e.target.value)}/>
                     <br/>
-                    <input type="password" placeholder="confirm password" required onChange={(e)=>setConfirm(e.target.value)}/>
+                    <input type="password" value={confirm} placeholder="confirm password" required onChange={(e)=>setConfirm(e.target.value)}/>
                     <br/>
-                    <button onChange={clk}>submit</button>
+                    <button onClick={clk}>submit</button>
+                    <h1>{mes}</h1>
                 </div>
             </form>
         </div>
