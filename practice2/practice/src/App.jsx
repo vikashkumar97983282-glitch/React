83b96348f63c3727925fa88953d1534adf8d54f7
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-04-08 23:31:45 +0530

    this is practice using built api.

---
+++ practice2/practice/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        <h1>hello programmer's</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
