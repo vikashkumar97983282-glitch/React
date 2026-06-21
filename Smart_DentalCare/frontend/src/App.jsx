b01cd4937b0df597b9b3f67edbb65fbdd9800f3a
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-06-21 10:13:23 +0530

    smart dental care project.

---
+++ Smart_DentalCare/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold bg-blue-500 text-white p-4">hello Govinda</h1>
+        <p>welcome to our website</p>
+      </div>
+    </>
+  )
+}
+
+export default App
