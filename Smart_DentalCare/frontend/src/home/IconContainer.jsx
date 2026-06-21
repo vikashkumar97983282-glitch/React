82bc8bac158036d3c270835e30ed2cc7454b708a
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-06-21 14:48:33 +0530

    add all function and icons .

--- Smart_DentalCare/frontend/src/home/IconContainer.jsx
+++ Smart_DentalCare/frontend/src/home/IconContainer.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-
-function IconContainer() {
+function IconContainer(props) {
     return (
-        <div className="flex justify-center items-center space-x-4 bg-gray-400 p-4 h-50 w-50 rounded-lg m-4">
-            
+        <div className={`flex justify-center items-center space-x-4 ${props.bgColor} p-30 h-30 w-80 rounded-lg ml-6 mt-6`}>
+            <h1 className="text-2xl font-bold">{props.name}</h1>
+            <i className={`${props.icon} ${props.iconColor} text-4xl`}></i>    
         </div>
     )
 }
--- Smart_DentalCare/frontend/src/home/homepage.jsx
+++ Smart_DentalCare/frontend/src/home/homepage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./header";
 import IconContainer from "./IconContainer";
 
@@ -6,21 +5,21 @@ import IconContainer from "./IconContainer";
 function HomePage() {
 
     let icons = [
-        { name: " New Patients", icon: "fa-solid fa-user" },
-        { name: " Patients List", icon: "fa-solid fa-user" },
-        { name: "Appointments", icon: "fa-solid fa-calendar" },
-        { name: "Billing", icon: "fa-solid fa-file-invoice-dollar" },
-        { name: "Reports", icon: "fa-solid fa-tooth" },
-        { name: "Settings", icon: "fa-solid fa-tooth" },
+        { name: " New Patients", icon: "fa-solid fa-user", iconColor: "text-blue-600", bgColor:"bg-blue-300"},
+        { name: " Patients List", icon: "fa-solid fa-users", iconColor: "text-violet-600", bgColor:"bg-violet-300"},
+        { name: "Appointments", icon: "fa-solid fa-calendar", iconColor: "text-yellow-600", bgColor:"bg-yellow-200"},
+        { name: "Billing", icon: "fa-solid fa-file-invoice-dollar", iconColor: "text-green-700", bgColor:"bg-green-300"},
+        { name: "Reports", icon: "fa-solid fa-tooth", iconColor: "text-red-600", bgColor:"bg-red-300"},
+        { name: "Settings", icon: "fa-solid fa-gear", iconColor: "text-gray-700", bgColor:"bg-gray-300"},
     ];
 
     return (
         <div>
             {/* <h1>Home Page</h1> */}
             <Header />
-            <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-wrap justify-start items-center ml-25 p-4">
                 {icons.map((item,idx)=>{
-                    return (<IconContainer key={idx} name={item.name} icon={item.icon} />  )
+                    return (<IconContainer key={idx} name={item.name} icon={item.icon} iconColor={item.iconColor} bgColor={item.bgColor} />  )
                 })}
             </div>
         </div>
