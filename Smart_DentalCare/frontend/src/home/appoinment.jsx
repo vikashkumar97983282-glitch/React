401d07c6ba820229f46e4001bfda06396d624f61
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-07-08 15:11:52 +0530

    add appoinment page.

--- Smart_DentalCare/frontend/src/home/appoinment.jsx
+++ Smart_DentalCare/frontend/src/home/appoinment.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 function Appoinment() {
     return(
-        <div className="flex justify-start items-center h-12 ml-40 mr-40 m-10 ">
-            <h1 className="font-bold text-lg p-2">Today's Appoinments </h1>
+        <div >
+            <div class="rounded-lg shadow-md  ml-40 mr-40 mt-10">
+            <div class="flex justify-between items-center mb-4">
+                <h2 class="text-lg font-semibold">Recent Items</h2>
+                <a href="#" class="text-blue-500 hover:text-blue-700 text-sm font-medium">
+                View All
+                </a>
+            </div>
+
+            
+            <ul class="space-y-3">
+                <li class="p-3 border rounded-md hover:bg-gray-50">
+                Item 1
+                </li>
+                <li class="p-3 border rounded-md hover:bg-gray-50">
+                Item 2
+                </li>
+                <li class="p-3 border rounded-md hover:bg-gray-50">
+                Item 3
+                </li>
+                <li class="p-3 border rounded-md hover:bg-gray-50">
+                Item 4
+                </li>
+            </ul>
+
+            </div>
         </div>
     )
 }
